da929ef93e2505a58085cb13bd7f4a35221f1e8b
Vladimir Collak <vlad@collak.net>
2015-09-19 18:44:16 -0500

    utilities file

---
+++ utilities.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"strconv"
+	"time"
+)
+
+//converts the UNIX epoch string to a time
+func msToTime(ms string) (time.Time, error) {
+	msInt, err := strconv.ParseInt(ms, 10, 64)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Unix(0, msInt*int64(time.Millisecond)), nil
+}
